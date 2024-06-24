705ac36de3757375aa1d8c246ad03034022e334b
programmerhasan001 <programmermdhasan@gmail.com>
2024-06-24 20:08:16 +0600

    added svg anim

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let path = `M 10 200 Q 965 200 1910 200`;
 let finalPath = `M 10 200 Q 965 200 1910 200`;
+let cursor = document.querySelector("#cursor")
 
 const string = document.querySelector("#string")
 string.addEventListener("mousemove", (event) => {
@@ -16,5 +17,14 @@ string.addEventListener("mouseleave", (event) => {
         attr: {d: finalPath},
         duration: 1.3,
         ease: "elastic.inOut(1,0.2)"
+    })
+})
+
+
+window.addEventListener("mousemove", (e) => {
+    gsap.to(cursor, {
+        x: e.x - 7,
+        y: e.y - 7,
+        duration: 0.3
     })
 })
